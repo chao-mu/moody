7ecaf350f5c940dbd60be635f89e59766a7b48f9
Danimal <chao-mu@hackpoetic.com>
2022-02-23 02:08:53 +0000

    add line graph for senators

--- moody/dashboard.py
+++ moody/dashboard.py
@@ -7,6 +7,9 @@ from sqlalchemy.sql import text
 # Ours
 from moody.db import db, TwitterSentiment
 
+# Core
+import json
+
 dashboard = Blueprint('dashboard', __name__,
         url_prefix='/', template_folder='templates')
 
@@ -24,17 +27,31 @@ def index():
     sentiment_results = db.session.query(TwitterSentiment).filter_by(
         sentiment=requested_sentiment).order_by(TwitterSentiment.week_of).all()
 
-    sentiments_by_tag = {}
+    sentiments_by_tag_x = {}
+    sentiments_by_tag_y = {}
+    tag_names = {
+        "Republican (US Political Party)": "Republican",
+        "Democrat (US Political Party)": "Democrat",
+    }
     for sentiment in sentiment_results:
-        if sentiment.tag not in sentiments_by_tag:
-            sentiments_by_tag[sentiment.tag] = []
+        if sentiment.tag not in tag_names:
+            continue
 
-        sentiments_by_tag[sentiment.tag].append(
-            (sentiment.week_of, sentiment.ratio))
+        tag = tag_names[sentiment.tag]
+
+        if tag not in sentiments_by_tag_x:
+            sentiments_by_tag_x[tag] = []
+            sentiments_by_tag_y[tag] = []
+
+        sentiments_by_tag_x[tag].append(sentiment.week_of)
+        sentiments_by_tag_y[tag].append(sentiment.ratio)
 
     if requested_sentiment == "POSITIVE":
         title = "Positivity Index"
     else:
         title = "Negativity Index"
 
-    return render_template("index.html.j2", sentiments=sentiments_by_tag, title=title)
+    return render_template("index.html.j2",
+            sentiments_x=json.dumps(sentiments_by_tag_x, default=str),
+            sentiments_y=json.dumps(sentiments_by_tag_y, default=str),
+            title=title)
